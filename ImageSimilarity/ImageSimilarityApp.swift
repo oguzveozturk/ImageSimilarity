6b508b320a90c329053380c9ba97dbf78f66dd94
Oguz <oguzveozturk@gmail.com>
2022-10-06 19:20:02 +0300

    Initial Commit

---
+++ ImageSimilarity/ImageSimilarityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSimilarityApp.swift
+//  ImageSimilarity
+//
+//  Created by Oğuz Öztürk on 6.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageSimilarityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
